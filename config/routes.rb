aa56381049e37be82f8be27eb5b86c0534056907
mpkuczer <mpkuczer@gmail.com>
2022-07-04 00:32:53 +0200

    created and migrated db, restored controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
-# Rails.application.routes.draw do
-#   devise_for :users
-#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-#   resources :games, only: [:show, :new, :create, :edit, :update]
-#   resources :positions, only: [:show, :new, :create, :edit, :update]
-#   # Defines the root path route ("/")
-#   root to: 'pages#home'
-# end
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :games, only: [:show, :new, :create, :edit, :update]
+  resources :positions, only: [:show, :new, :create, :edit, :update]
+  # Defines the root path route ("/")
+  root to: 'pages#home'
+end
