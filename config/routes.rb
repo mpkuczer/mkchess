4def6a06e4bf5ef9a69874c5a0cddf0b5f9ec152
mpkuczer <mpkuczer@gmail.com>
2022-07-04 00:22:01 +0200

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :games, only: [:show, :new, :create, :edit, :update]
+  resources :positions, only: [:show, :new, :create, :edit, :update]
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'pages#home'
 end
