a0c7c33c88c0c9675b5fe94758749e5574628389
mpkuczer <mpkuczer@gmail.com>
2022-07-04 03:29:15 +0200

    refactor router

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,9 @@
 class GamesController < ApplicationController
-  before_action: :authenticate_user!, except: [:index, :show, :guest_new]
+  before_action :authenticate_user!, except: [:index, :show, :guest]
   def show
   end
 
-  # def new
-  #   @game = Game.new
-  # end
+  def guest
+    @game = Game.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :games, only: [:show, :new, :create, :edit, :update]
+  resources :games, only: [:show, :new, :create, :edit, :update] do
+    collection do
+      get 'guest'
+    end
   resources :positions, only: [:show, :new, :create, :edit, :update]
   resources :challenges, only: [:index, :show, :new, :create, :edit, :update]
+    member do
+      get 'respond'
+    end
   get '/games/guest', to: 'games#guest'
   get '/challenges/:id/respond', to: 'challenges#respond'
   # Defines the root path route ("/")
