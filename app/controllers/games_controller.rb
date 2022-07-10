570a1023d8628fa6fbf3b98897d8489621f4cc47
mpkuczer <mpkuczer@gmail.com>
2022-07-10 03:45:34 +0200

    style

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  # before_action :authenticate_user!, except: [:index, :show, :pass_and_play, :new, :create]
+  # before_action :authenticate_user!, except: [:index, :show, :new, :create]
   protect_from_forgery except: :skip_to_position
 
   def show
@@ -28,26 +28,22 @@ class GamesController < ApplicationController
 
     if @position == @game.positions.order(:order).last
       if @position.validate_move(params[:i1].to_i,
-                                params[:j1].to_i,
-                                params[:i2].to_i,
-                                params[:j2].to_i)
+                                 params[:j1].to_i,
+                                 params[:i2].to_i,
+                                 params[:j2].to_i)
         fen = @position.to_fen(params[:i1].to_i,
                               params[:j1].to_i,
                               params[:i2].to_i,
                               params[:j2].to_i)
-        if @game.positions.build(fen: fen, order: @position.order + 1).save
-          @new_position = @game.positions.last
-          @move = [params[:i1].to_i,
-                  params[:j1].to_i,
-                  params[:i2].to_i,
-                  params[:j2].to_i]
-          respond_to do |format|
-            format.js { render 'games/new', layout: false, locals: { position: @new_position } }
-          end
-        else
-          render json: { error: fen }
+        @game.positions.build(fen: fen, order: @position.order + 1).save
+        @new_position = @game.positions.last
+        @move = [params[:i1].to_i,
+                params[:j1].to_i,
+                params[:i2].to_i,
+                params[:j2].to_i]
+        respond_to do |format|
+          format.js { render 'games/new', layout: false, locals: { position: @new_position } }
         end
-        
       else
         render json: { error: "Invalid move" }
       end
@@ -59,28 +55,20 @@ class GamesController < ApplicationController
   def skip_to_position
     @position = Position.find(params[:position_id].to_i)
     @game = @position.game
-    @new_position = @game.positions.order(:order).first
-    respond_to do |format|
-      format.js { render 'games/new', layout: false, locals: { position: @new_position } }
-      format.html { render 'games/new', layout: false, locals: { position: @new_position } }
+    case params[:option].to_sym
+    when :start
+      @new_position = @game.positions.order(:order).first
+    when :previous
+      @new_position = @position.order == 1 ? @position : @game.positions.find_by(order: @position.order - 1)
+    when :next
+      byebug
+      @new_position = @position.order == @game.positions.count ? @position : @game.positions.find_by(order: @position.order + 1)
+    when :current
+      @new_position = @game.positions.order(:order).last
     end
-  end
-
-  def pass_and_play
-    @player_one = Guest.create
-    @player_two = Guest.create
-    colors = [@player_one, @player_two].shuffle
-    @game = Game.new(white_id: colors[0].id,
-                     black_id: colors[1].id,
-                     white_type: colors[0].class.name,
-                     black_type: colors[1].class.name,
-                     status: 0)
-    @game.positions.build.save
-    if @game.save 
-      redirect_to game_path(@game)
-    else
-      render :index, status: :unprocessable_entity
-    end
+      respond_to do |format|
+        format.js { render 'games/new', layout: false, locals: { position: @new_position } }
+      end
   end
 
   private
